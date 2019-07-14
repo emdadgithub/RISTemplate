b10674cf91bfefc9935757116df807dc69358e1c
emdad <kanon.ice.ru@gmail.com>
2019-07-14 11:36:40 +0600

    update RIS template form

--- src/app/ris-template/ris-template.component.ts
+++ src/app/ris-template/ris-template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/primeng';
 
 @Component({
   selector: 'app-ris-template',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RisTemplateComponent implements OnInit {
 
+  controlTypes: SelectItem[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.controlTypes = [];
+    // this.genders.push({ label: "Select Gender", value: "" });
+    this.controlTypes.push({ label: "Text", value: "T" });
+    this.controlTypes.push({ label: "Text Area", value: "A" });
+    this.controlTypes.push({ label: "Check Box", value: "C" });
+    this.controlTypes.push({ label: "Radio Button", value: "R" });
+    this.controlTypes.push({ label: "Drop Down List", value: "D" });
+    this.controlTypes.push({ label: "Editor", value: "E" });
   }
 
 }
