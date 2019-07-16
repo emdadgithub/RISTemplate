d2c373d362933cacbca4c7508f3d01ad28253035
Bipon Biswas <bipon770@gmail.com>
2019-07-16 10:41:18 +0600

    merge file

--- src/app/dynamic-form-builder/dynamic-form-builder.module.ts
+++ src/app/dynamic-form-builder/dynamic-form-builder.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DynamicFormBuilderComponent } from './dynamic-form-builder.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FieldBuilderComponent } from './field-builder/field-builder.component';
 import { CheckboxComponent } from './dynamic-controls/checkbox/checkbox.component';
 import { DropdownComponent } from './dynamic-controls/dropdown/dropdown.component';
@@ -9,18 +9,20 @@ import { FileComponent } from './dynamic-controls/file/file.component';
 import { RadioComponent } from './dynamic-controls/radio/radio.component';
 import { TextboxComponent } from './dynamic-controls/textbox/textbox.component';
 import {ButtonModule} from 'primeng/button';
-import { RadioButtonModule, DropdownModule, CheckboxModule, InputTextModule, PanelModule } from 'primeng/primeng';
+import { RadioButtonModule, DropdownModule, CheckboxModule, InputTextModule, PanelModule, EditorModule } from 'primeng/primeng';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FormsModule,
     RadioButtonModule,
     DropdownModule,
     CheckboxModule,
     InputTextModule,
     ButtonModule,
-    PanelModule
+    PanelModule,
+    EditorModule
   ],
   declarations: [
      DynamicFormBuilderComponent,
